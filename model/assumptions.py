7b4f577974b5f56592c8632926cc4ab9c34eb638
Daniel Wu <danielw72908@gmail.com>
2026-08-03 22:47:43 -0400

    chore: update forecast assumptions in model/assumptions.py

--- model/assumptions.py
+++ model/assumptions.py
@@ -7,12 +7,12 @@ def get_forecast_assumptions(historical_data: pd.DataFrame) -> Dict[str, object]
     return {
         "forecast_years": 5,
         "forecast": {
-            "revenue_growth_rate": historical_data["revenue_growth_rate"].mean(),
-            "operating_margin": historical_data["operating_margin"].mean(),
-            "tax_rate": historical_data["taxRate"].mean(),
-            "daPercentRevenue": historical_data["daPercentRevenue"].mean(),
-            "capexPercentRevenue": historical_data["capexPercentRevenue"].mean(),
-            "nwcPercentRevenue": historical_data["nwcPercentRevenue"].mean(),
+            "revenue_growth_rate": [0.06, 0.055, 0.05, 0.04, 0.03],
+            "operating_margin": [0.31, 0.312, 0.315, 0.317, 0.32],
+            "tax_rate": [0.16, 0.16, 0.16, 0.16, 0.16],
+            "daPercentRevenue": [0.029, 0.029, 0.0285, 0.028, 0.028],
+            "capexPercentRevenue": [0.031, 0.03, 0.03, 0.029, 0.029],
+            "nwcPercentRevenue": [-0.14, -0.14, -0.139, -0.138, -0.138],
         },
         "valuation": {
             "WACC": 0.1091,
